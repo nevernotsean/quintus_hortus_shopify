7a67205628163cc4f9b8a3f17b2aff9bd31cd6fb
Sean Harrington <sean@never-not.com>
2017-05-08 21:11:05 -0400

    switch to single product, multiple screens
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- live-theme/assets/app.fullpage.js
+++ live-theme/assets/app.fullpage.js
@@ -4,184 +4,201 @@ var fullpage = {
   init: function() {
     fullpage.container = $("#fullpage");
 
-    if ($('body').attr('id') === 'le-concept') {
-      fullpage.options = {
-        afterLoad: function(anchorLink, index) {
-          // console.log('afterLoad')
-        },
-        onLeave: function(index, nextIndex, direction) {
-          // console.log('onLeave')
-        },
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {
-          console.log('onSlideLeave', anchorLink, index, slideIndex, direction, nextSlideIndex);
-        },
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
-          console.log('afterSlideLoad', anchorLink, index, slideAnchor, slideIndex)
-
-        }
+    if ($("body").attr("id") === "le-concept") {
+      //
+    } else if ($("body.template-product").length) {
+        fullpage.productView.init($("#product-form-container"));
+    } else if ($("body.template-article").length) {
+        fullpage.blogView.init();
+    } else {
+      if (fullpage.container.length) {
+        fullpage.container.fullpage(fullpage.options);
       }
-    }
-    if ( fullpage.container.length ){
-      fullpage.container.fullpage(fullpage.options);
-    }
-
-    if ( $('body.template-product').length ){
-      fullpage.options = {
-        afterLoad: function(anchorLink, index) {
-          // console.log('afterLoad')
-        },
-        onLeave: function(index, nextIndex, direction) {
-          // console.log('onLeave')
-        },
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {
-          console.log('onSlideLeave', anchorLink, index, slideIndex, direction, nextSlideIndex);
-        },
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
-          console.log('afterSlideLoad', anchorLink, index, slideAnchor, slideIndex)
-        }
-      }
-      $('.product-slide').each( function () {
-        fullpage.productView.init( $(this) )
-      })
-    }
-
-    if ( $('body.template-article') ){
-      fullpage.blogView.init();
-    }
-  },
-  blogView: {
-    init: function(){
-      var self = fullpage.blogView
-      $('body').scrollTop(0)
-
-      $(window).scroll(function(){
-        var active = false
-        if (window.pageYOffset > 10 && !active){
-          $('header > .bg').addClass('active')
-          active = true
-        } else {
-          $('header > .bg').removeClass('active')
-          active = false
-        }
-      })
     }
   },
   productView: {
     options: null,
     $productContainer: [],
-    init(productContainer){
+    init(productContainer) {
+      var self = fullpage.productView, productHandle;
 
-      var self = fullpage.productView, productHandle
+      self.$productContainer = $(productContainer);
 
-      self.$productContainer = $(productContainer)
+      productHandle = self.$productContainer.data("handle");
 
-      productHandle = self.$productContainer.data('handle')
+      window.MoneyFormat = $(".MoneyFormat")[0].value;
 
-      window.MoneyFormat = $('.MoneyFormat')[0].value
-
-      options = new Shopify.OptionSelectors('productSelect', {
+      selectOptions = new Shopify.OptionSelectors("productSelect", {
         product: window.ProductJSON[productHandle],
         onVariantSelected: self.selectCallback,
         enableHistoryState: true
       });
 
-      self.addEventListeners()
+      self.addEventListeners();
+
+      fullpage.options = {
+        afterLoad: function(anchorLink, index) {
+          // console.log('afterLoad')
+        },
+        onLeave: function(index, nextIndex, direction) {
+          // console.log('onLeave')
+        },
+        onSlideLeave: function(
+          anchorLink,
+          index,
+          slideIndex,
+          direction,
+          nextSlideIndex
+        ) {
+          console.log(
+            "onSlideLeave",
+            anchorLink,
+            index,
+            slideIndex,
+            direction,
+            nextSlideIndex
+          );
+        },
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+          console.log(
+            "afterSlideLoad",
+            anchorLink,
+            index,
+            slideAnchor,
+            slideIndex
+          );
+        }
+      }
+      if (fullpage.container.length) {
+        fullpage.container.fullpage(fullpage.options);
+      }
     },
     addEventListeners: function() {
       var self = fullpage.productView,
-          $rangeInput = self.$productContainer.find('.range input')
+        $rangeInput = self.$productContainer.find(".range input");
 
       // sync the quanitities
       self.$productContainer.find("#Quantity").on("change", function() {
-        self.$productContainer.find("#AddToCart").attr("data-cart-quantity", $(this).val());
-      });self.$productContainer
+        self.$productContainer
+          .find("#AddToCart")
+          .attr("data-cart-quantity", $(this).val());
+      });
+      self.$productContainer;
 
       // Update the price and position of range, amount and price label
-      $rangeInput.on('input', function(e) {
+      $rangeInput.on("input", function(e) {
         var qty = e.target.value,
-            baseprice = self.$productContainer.find('#ProductPrice').attr('content'),
-            price = (qty / e.target.step * baseprice).toFixed(2)
+          baseprice = self.$productContainer
+            .find("#ProductPrice")
+            .attr("content"),
+          price = (qty / e.target.step * baseprice).toFixed(2);
 
-        self.$productContainer.find("#price-tooltip span").html(price)
-        self.$productContainer.find('#amount-tooltip span').html(qty + "g")
+        self.$productContainer.find("#price-tooltip span").html(price);
+        self.$productContainer.find("#amount-tooltip span").html(qty + "g");
 
-        var max = e.target.max,
-            min = e.target.min,
-            pct = (qty / max * 100) - 5;
+        var max = e.target.max, min = e.target.min, pct = qty / max * 100 - 5;
 
-            pct = pct < 0 ? 0 : pct
+        pct = pct < 0 ? 0 : pct;
 
-
-            self.$productContainer.find("#price-tooltip, #amount-tooltip").css("left", pct + "%")
-            self.$productContainer.find('#bar').css("width", "calc(" + pct + "% + 12px)")
+        self.$productContainer
+          .find("#price-tooltip, #amount-tooltip")
+          .css("left", pct + "%");
+        self.$productContainer
+          .find("#bar")
+          .css("width", "calc(" + pct + "% + 12px)");
       });
 
       // on range movement settles, change the quantity
-      $rangeInput.on('change', function(e) {
-        var newVal = e.target.value / e.target.step
-        var qtyInput = document.getElementById('Quantity')
-        qtyInput.value = newVal
-        self.$productContainer.find("[data-cart-quantity]").attr('data-cart-quantity', newVal)
+      $rangeInput.on("change", function(e) {
+        var newVal = e.target.value / e.target.step;
+        var qtyInput = document.getElementById("Quantity");
+        qtyInput.value = newVal;
+        self.$productContainer
+          .find("[data-cart-quantity]")
+          .attr("data-cart-quantity", newVal);
 
         self.test(qtyInput.value, newVal);
       });
 
       // selector changes
-      self.$productContainer.find('.product-size button').click(function(e) {
-        var $this = $(e.target)
-        var id = $this.attr('data-id')
-        options.selectVariant(id)
-        self.$productContainer.find('.product-size button').removeClass('selected')
-        $this.addClass('selected')
-        $rangeInput.val($rangeInput.attr("min")).trigger('input');
-        self.updatePrice()
-      })
+      self.$productContainer.find(".product-size button").click(function(e) {
+        var $this = $(e.target);
+        var id = $this.attr("data-id");
+        options.selectVariant(id);
+        self.$productContainer
+          .find(".product-size button")
+          .removeClass("selected");
+        $this.addClass("selected");
+        $rangeInput.val($rangeInput.attr("min")).trigger("input");
+        self.updatePrice();
+      });
       // submit the form
-      self.$productContainer.find('#submitButton').click(function(e) {
-        e.preventDefault()
-        self.$productContainer.find('#AddToCart').trigger('click');
-        self.$productContainer.find('#AddToCartForm').submit();
-      })
+      self.$productContainer.find("#submitButton").click(function(e) {
+        e.preventDefault();
+        self.$productContainer.find("#AddToCart").trigger("click");
+        self.$productContainer.find("#AddToCartForm").submit();
+      });
 
       // products ajax
       // self.$productContainer.find('[ref="product-select"]').click(function(e) {
-        // e.preventDefault()
-        // var target = $(this),
-            // productURl = this.href
+      // e.preventDefault()
+      // var target = $(this),
+      // productURl = this.href
 
-        // self.loadProduct(productURl)
+      // self.loadProduct(productURl)
       // })
     },
-    // loadProduct: function(productUrl) {
-    //   $('#product-container').load( productUrl + ' #product-container *', function(data){
-    //     console.log(data)
-    //   })
-    // },
+    loadProduct: function(productUrl) {
+      $("#product-form-container").load(
+        productUrl + " #product-form-container *",
+        function(data) {
+          console.log(data);
+        }
+      );
+    },
     selectCallback: function(variant, selector) {
-
       console.log("Switching variant: ", variant);
-
-      concrete.switchVariant({
-        moneyFormat: window.MoneyFormat,
-        variant: variant,
-      }, self.$productContainer)
+      concrete.switchVariant(
+        {
+          moneyFormat: window.MoneyFormat,
+          variant: variant
+        },
+        self.$productContainer
+      );
     },
     updatePrice: function() {
-      var price = self.$productContainer.find('#ProductPrice').attr('content')
-      price = Number(price).toFixed(2)
-      self.$productContainer.find('#price-tooltip span').html(price)
+      var price = self.$productContainer.find("#ProductPrice").attr("content");
+      price = Number(price).toFixed(2);
+      self.$productContainer.find("#price-tooltip span").html(price);
     },
-    test: function(val1, val2){
-      val2 = val2.toString()
-      if (val1 !== val2){
-        debugger
+    test: function(val1, val2) {
+      val2 = val2.toString();
+      if (val1 !== val2) {
+        debugger;
       } else {
-        console.log('Test: ', val1, val2)
+        console.log("Test: ", val1, val2);
       }
+    }
+  },
+  blogView: {
+    init: function() {
+      var self = fullpage.blogView;
+      $("body").scrollTop(0);
+
+      $(window).scroll(function() {
+        var active = false;
+        if (window.pageYOffset > 10 && !active) {
+          $("header > .bg").addClass("active");
+          active = true;
+        } else {
+          $("header > .bg").removeClass("active");
+          active = false;
+        }
+      });
     }
   }
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
   fullpage.init();
-})
+});
